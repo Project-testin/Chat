451c4ba7e46cd1915b04d72b544062766459bf40
Project-testin <44239149+Project-testin@users.noreply.github.com>
2018-10-17 18:06:53 +0100

    Add files via upload

---
+++ Server testing code/cmake-build-debug/CMakeFiles/Server_testing_code.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Server_testing_code.dir/main.c.o"
+  "Server_testing_code.pdb"
+  "Server_testing_code"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Server_testing_code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
